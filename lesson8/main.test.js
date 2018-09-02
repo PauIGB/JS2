5557a083fd5cf2b020fd0c6eff8a497c7dc4a02c
PauIGB <56a06@mail.ru>
2018-09-02 19:55:49 +0300

    added checking for name

--- lesson8/main.test.js
+++ lesson8/main.test.js
@@ -1,33 +1,20 @@
 describe("В поле name могут быть только русские и ангийские буквы, строка не может начинаться с пробела", () => {
-     it('Проверка на русские и английские буквы и пробелы между словами', () => {        
-           assert.equal(check(/^([a-zа-яё].{1})(([a-zа-яё])|(\s))*([a-zа-яё].{0})$/im, "Петр The destroyer Сидоров"), true)      
-    })
-    it('Проверка на цифры', () => {        
-           assert.equal(check(/^([a-zа-яё].{1})(([a-zа-яё])|(\s))*([a-zа-яё].{0})$/im, 213412), false)      
+    const template = /^([a-zа-яё].{1})(([a-zа-яё])|(\s))*([a-zа-яё].{0})$/im
+    it('Проверка на русские и английские буквы и пробелы между словами', () => {        
+           assert.equal(check(template, "Петр The destroyer Сидоров"), true)      
     })
     it('Проверка на пробел в начале строки', () => {        
-           assert.equal(check(/^([a-zа-яё].{1})(([a-zа-яё])|(\s))*([a-zа-яё].{0})$/im, " asdfsad"), false)      
+           assert.equal(check(template, " asdfsad"), false)      
     })
     it('Проверка на пробел в конце строки', () => {        
-       assert.equal(check(/^([a-zа-яё].{1})(([a-zа-яё])|(\s))*([a-zа-яё].{0})$/im, "asdfsad "), false)      
+       assert.equal(check(template, "asdfsad "), false)      
     })
-    it('Проверка на спецсимволы', () => {        
-       assert.equal(check(/^([a-zа-яё].{1})(([a-zа-яё])|(\s))*([a-zа-яё].{0})$/im, "< "), false)      
-    })
+    const symbols = "0123456789 [$&+,:;=?@#|'<>.-^*()%!]{}" 
+    for (i = 0; i < symbols.length; i++) {
+        const symbol = symbols.charAt(i);
+        it(`Проверка на наличие символа ${symbol} в поле name`, () => {        
+           assert.equal(check(template, symbol), false)      
+        })
+    }
 
 })
-//describe("В поле name могут быть только русские и ангийские буквы, строка не может начинаться с пробела", () => {
-//    var text = "";
-//    function check(template, value) {        
-//        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 [$&+,:;=?@#|'<>.-^*()%!]{} "; 
-//        for (var i = 0; i < 5; i++) {
-//            text += possible.charAt(Math.floor(Math.random() * possible.length));
-//        }             
-//        it(`проверка выражения ${text}`, () => {        
-//           assert.equal(check(template, value), false)})
-//        }
-//        for (var i = 0; i < 5; i++) {
-//            check(/^([a-zа-яё].{1})(([a-zа-яё])|(\s))*([a-zа-яё].{0})$/im, text);
-//            }  
-//})
-//   
