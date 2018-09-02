464b718460744015fa3f2fc3d2f120d500103493
PauIGB <56a06@mail.ru>
2018-09-02 20:03:58 +0300

    phone added

--- lesson8/main.test.js
+++ lesson8/main.test.js
@@ -9,12 +9,28 @@ describe("В поле name могут быть только русские и а
     it('Проверка на пробел в конце строки', () => {        
        assert.equal(check(template, "asdfsad "), false)      
     })
-    const symbols = "0123456789 [$&+,:;=?@#|'<>.-^*()%!]{}" 
+    const symbols = "0123456789 [$&+,:;=?@#|'<>.-^*()%!]{   }" 
     for (i = 0; i < symbols.length; i++) {
         const symbol = symbols.charAt(i);
         it(`Проверка на наличие символа ${symbol} в поле name`, () => {        
            assert.equal(check(template, symbol), false)      
         })
     }
-
+})
+describe("В поле phone возможна запись телефона только в следующем формате: +7(000)000-0000", () => {
+    const template = /^((\+\d)(\(\d{3}\))(\d{3})-(\d{4}))$/m
+    it('Проверка телефона в формате: +7(000)000-0000', () => {        
+           assert.equal(check(template, '+7(000)000-0000'), true)      
+    })
+    it('Проверка на наличие пробелов', () => {        
+           assert.equal(check(template, " "), false)      
+    })
+   
+    const symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz   [$&+,:;=?@#|'<>.-^*()%!]{}" 
+    for (i = 0; i < symbols.length; i++) {
+        const symbol = symbols.charAt(i);
+        it(`Проверка на наличие символа ${symbol} в поле phone`, () => {        
+           assert.equal(check(template, symbol), false)      
+        })
+    }
 })
